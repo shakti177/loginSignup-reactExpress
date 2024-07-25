bdbef3372b6d91289ed576f1f1c944aa1777a517
Shakti Tamrakar <57393413+shakti177@users.noreply.github.com>
2024-07-25 16:53:56 +0530

    Update SignUp.jsx

--- client/src/Components/SignUp.jsx
+++ client/src/Components/SignUp.jsx
@@ -3,7 +3,6 @@ import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import "./SignUp.css";
 
 const SignUp = () => {
   const [name, setName] = useState("");
