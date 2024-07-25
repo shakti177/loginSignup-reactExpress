30554063fd93b549dece14e7bdc6c8d262da6ccb
Shakti Tamrakar <57393413+shakti177@users.noreply.github.com>
2024-07-25 17:05:34 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
   origin: 'https://login-signup-frontend-one.vercel.app',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
